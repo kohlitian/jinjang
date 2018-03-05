6f39650cdbb96914c9872733d6428fc33054d554
Armin Nikdel <sanarena@gmail.com>
2018-03-05 22:52:42 +0800

    Base structure of index

--- index.php
+++ index.php
@@ -1,1 +1,64 @@
-<?php echo "Hello WORLD!"; ?>
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+	<title>Home</title>
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+	<meta http-equiv="content-Type" content="text/html; charset=utf-8">
+	<meta http-equiv="content-language" content="en">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/jinjang.css">
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+</head>
+
+<body class="Site">
+
+	<!--slide show-->
+	<div>
+		<div id="newCarousel" class="carousel slide" data-ride="carousel">
+			<ol class="carousel-indicators">
+				<li data-target="#newCarousel" data-slide-to="0" class="active"></li>
+				<li data-target="#newCarousel" data-slide-to="1"></li>
+				<li data-target="#newCarousel" data-slide-to="2"></li>
+			</ol>
+			<div class="carousel-inner">
+				<div class="item active">
+					<!-- by using div to display photos instead of <img>, we can benefit using CSS3 background-size: cover where background of a picture resize to available space to cover it -->
+					<div class="img" style="background-image: url('img/jinjang1.jpg');"></div></div>
+				<div class="item">
+					<div class="img" style="background-image: url('img/jinjang2.jpg');"></div></div>
+				<div class="item">
+					<div class="img" style="background-image: url('img/jinjang3.jpg');"></div></div>
+			</div>
+		<a class="left carousel-control" href="#newCarousel" data-slide="prev">
+			<span class="glyphicon glyphicon-chevron-left"></span>
+			<span class="sr-only">Previous</span>
+		</a>
+		<a class="right carousel-control" href="#newCarousel" data-slide="next">
+			<span class="glyphicon glyphicon-chevron-right"></span>
+			<span class="sr-only">Next</span>
+		</a>
+		</div>
+	</div>
+
+	<!--content-->
+	<div class="container marginTB">
+		<div class="row">
+			<div class="col-xs-12 col-md-6">
+				<h3>What is Jinjang Project?</h3>
+				<p>Jinjang project allow people from Jinjang community to utilize their free time and earn some money.</p>
+			</div>
+			<div class="col-xs-12 col-md-6">
+				<h3>What is e-business?</h3>
+				<p>e-business allow job providers to provide jobs for jinjang community job finders.
+					</p>
+			</div>
+		</div>
+	</div>
+
+
+	
+	
+	
+
+</body>
+</html>
