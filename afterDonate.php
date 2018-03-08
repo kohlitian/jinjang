d29e2d8cc3af67d2cdcfe05cda1dac2cf4c95d8c
kohlitian <ben5green@outlook.com>
2018-03-08 16:01:19 +0800

    donation (no design)

---
+++ afterDonate.php
@@ -0,0 +1,49 @@
+<?php
+//load startup scripts
+include("config.php");
+include("control.php");
+
+
+?>
+
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+	<title>Login</title>
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+	<meta http-equiv="content-Type" content="text/html; charset=utf-8">
+	<meta http-equiv="content-language" content="en">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/helpfit.css">
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+	
+</head>
+<body>
+	<!-- start of header -->
+	<?php 
+	include("header.php"); 
+	?>
+	<!-- end of header -->
+
+	<!--content-->
+	<div class="container marginTB">
+		<br>
+		<br>
+		<br>
+		<p><strong>Thank You for Donating to us, the money will process to other payment system and we will receive it soon. We will use the money to improve our system to help more people that live in jinjang utara. We will contact you if something happen, and you are welcome to contact us for more information. THANK YOU ! </strong></p>
+		<div class="clear"></div>
+		<br>
+		<br>
+		<br>
+	</div>
+
+<!-- start of footer code -->
+	<?php include("footer.php"); ?>
+	<!-- end of footer -->
+	
+	
+	
+	
+
+</body>
+</html>
---
+++ donate.php
@@ -0,0 +1,99 @@
+<?php
+//load startup scripts
+include("config.php" );
+include("control.php");
+$is_home=1;
+
+if($_SERVER['REQUIEST_METHOD'] == 'POST'){
+	$fullName = $_POST['fullName'];
+	$email = $_POST['email'];
+	$contactNo = $_POST['contactNo'];
+	$amount = $_POST['amount'];
+
+	if($amount<=0){
+		$amountError = "Sorry amount cannot be less than or equal to 0.";
+	} else {
+
+		echo "confirm('Are you sure you want to join this ".$row['title']." by ".$trainer['fullName']." for RM".$row['fee']."?')\"; >";
+
+
+		bootbox.confirm({message: "Are you sure you want to donate " + amount + " for us ?", 
+			buttons: { confirm: { label: 'Yes', className: 'btn-success'}, 
+			cancel: {label: 'No', className: 'btn-danger'}},
+			Callback: function(result){
+				header("Location: afterDonate.php");
+			}});
+	}
+}
+
+?>
+
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+	<title>Home</title>
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+	<meta http-equiv="content-Type" content="text/html; charset=utf-8">
+	<meta http-equiv="content-language" content="en">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/helpfit.css">
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+</head>
+<body class="Site">
+	<!-- start of header -->
+	<?php 
+	include("header.php"); 
+	?>
+	<!-- end of header -->
+	
+	<!--donate-->
+
+	<div class="container marginTB">
+		<br>
+		<br>
+		<br>
+		<form class="col-xs-offset-1 col-xs-10 col-sm-offset-2 col-sm-8 col-md-offset-3 col-md-6 col-lg-offset-3 col-lg-6" method="POST" action="LogIn.php">
+			<div class="formStyle">
+				<p><Strong>Donation Form</Strong></p>
+				<p>Please fill in your information</p>
+				<br>
+				<div class="input-group">
+				  	<span class="input-group-addon" id="basic-addon1"><label for="fullName"><i class="glyphicon glyphicon-user"></i></label></span>
+					<input type="text" required class="form-control" placeholder="Please enter your full name (can be empty)" id="fullName" name="fullName" value="<?php if(isset($user['fullName'])){echo $user['fullName'];} ?>">
+				</div>
+				<div class="input-group">
+					<span class="input-group-addon" id="basic-addon2"><label for="email"><i class="fa fa-key"></i></label></span>
+					<input class="form-control" required placeholder="Please enter your email" type="email" id="email" name="email" value="<?php if(isset($user['email'])){echo $user['email'];} ?>">
+				</div>
+				<div class="input-group">
+					<span class="input-group-addon" id="basic-addon2"><label for="contactNo"><i class="fa fa-key"></i></label></span>
+					<input class="form-control" required placeholder="Please enter your contact Number" type="tel" id="contactNo" name="contactNo" value="<?php if(isset($user['contactNo'])){echo $user['contactNo'];} ?>">
+				</div>
+				<div class="input-group noSpaceTop">
+					<span class="input-group-addon" id="basic-addon1"><label for="amount">RM</label></span>
+					<input class="form-control" type="number" name="amount" required id="amount" placeholder="Amount of donation"  value="<?php if (isset($_POST['amount'])&&$_POST['amount']) echo $_POST['amount']; ?>">
+					<span class="input-group-addon" id="basic-addon1" style="padding-bottom:0px;"><label for="number"><span>.00</span></label></span>
+				</div>
+				<button type="submit" class="btn btn-success btn-block btn-lg formButton">Log In</button>
+				<hr>
+				<div class="center">
+				<small>Or <a href="signup.php">sign up for a new account</a></small>
+				</div>
+			</div>
+		</form>
+		<div class="clear"></div>
+		<br>
+		<br>
+		<br>
+	</div>
+
+	<!-- start of footer code -->
+	<?php include("footer.php"); ?>
+	<!-- end of footer -->
+	
+	
+	
+	
+
+</body>
+</html>
