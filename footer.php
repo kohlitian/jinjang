02277c8c98963b210fee86e7ab00b4cfcdf7f764
Li Tian <ben5green@outlook.com>
2021-05-06 11:07:19 +0800

    change script to top of the page on footer

--- footer.php
+++ footer.php
@@ -1,26 +1,3 @@
-<div class="footer-gap-provider"></div>
-
-<footer class="container-fluid">
-		<div class="footer">
-			<div class="container marginTB">
-				<div class="row">
-					<div class="col-xs-7 col-sm-offset-1"  style="line-height: 17pt;">
-						Jinjang Project, For a better life
-						<small class="hidden-xs"><br><?php
-
-						//count statistics of users and jobs and postings and print them
-						if ($connect){ echo @mysqli_fetch_array(mysqli_query($connect,"select count(*) from jobfinder;"))[0]; ?> job finders, <?php echo @mysqli_fetch_array(mysqli_query($connect,"select count(*) from jobprovider;"))[0]; ?> job providers, <?php echo @mysqli_fetch_array(mysqli_query($connect,"select count(*) from jobs;"))[0]; ?> job ads <?php } ?></small>
-						<br><small style="font-size: 8pt;"><a href="donate.php">Click here for donation</a></small>
-					</div>
-					<div class="col-sm-4 col-xs-5">
-						<p>Copyright © 2018 Jinjang Project<br/>
-						<small>Rights Reserved<span class="hidden-xs hidden-sm"> for A.Nikdel & L.T Koh</span></small></p>
-					</div>
-				</div>
-			</div>	
-		</div>	
-</footer>
-
 <!-- Put scripts at end of the page, so they don't slow down loading time -->
 <script type="text/javascript" src = "js/jquery.js"></script>
 <script type="text/javascript" src = "js/bootbox.min.js"></script>
@@ -51,3 +28,28 @@ if ($passThruMessage!=''){ ?>
 if (isset($connect)&&$connect)
 	mysqli_close($connect);
 ?>
+
+
+<div class="footer-gap-provider"></div>
+
+<footer class="container-fluid">
+		<div class="footer">
+			<div class="container marginTB">
+				<div class="row">
+					<div class="col-xs-7 col-sm-offset-1"  style="line-height: 17pt;">
+						Jinjang Project, For a better life
+						<small class="hidden-xs"><br><?php
+
+						//count statistics of users and jobs and postings and print them
+						if ($connect){ echo @mysqli_fetch_array(mysqli_query($connect,"select count(*) from jobfinder;"))[0]; ?> job finders, <?php echo @mysqli_fetch_array(mysqli_query($connect,"select count(*) from jobprovider;"))[0]; ?> job providers, <?php echo @mysqli_fetch_array(mysqli_query($connect,"select count(*) from jobs;"))[0]; ?> job ads <?php } ?></small>
+						<br><small style="font-size: 8pt;"><a href="donate.php">Click here for donation</a></small>
+					</div>
+					<div class="col-sm-4 col-xs-5">
+						<p>Copyright © 2018 Jinjang Project<br/>
+						<small>Rights Reserved<span class="hidden-xs hidden-sm"> for A.Nikdel & L.T Koh</span></small></p>
+					</div>
+				</div>
+			</div>	
+		</div>	
+</footer>
+
